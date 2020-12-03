4b168b9064cfe6f76737d65a2d2be90973405fc0
NYchicken <mirro97@naver.com>
2020-12-04 04:12:19 +0900

    mini project: js-calculator

--- mentoring-html/자바스크립트로 만드는 계산기/app.js
+++ mentoring-html/자바스크립트로 만드는 계산기/app.js
@@ -1,10 +1,56 @@
-const keys = document.querySelector(".calculator-keys");
-const input = document.querySelector(".calculator-display");
-const element = "";
+const output_value = document.getElementById('output-display');
+const input_value = document.getElementById('input-display');
+const value = document.querySelectorAll('.number');
+const operator = document.querySelectorAll('.operator');
 
-document.querySelectorAll('.number').forEach((element)=> {
-    element.addEventListener('click', (element) => {
-        input.innerText += element.innerText;
-        alert(input);
-    });
+const empty = (element) =>{
+    element.innerText = "";
+};
+
+document.getElementById('claer').addEventListener('click', function(){
+    empty(output_value);
+    empty(input_value);
 });
+
+document.getElementById('claer').addEventListener('click', function(){
+    input_value.innerText = "";
+});
+
+document.getElementById('dot').addEventListener('click', function(){
+    if(input_value.innerText.includes('.')) return;
+
+    input_value.innerText += this.innerText;
+});
+
+for(var i = 0; i<value.length; i++) {
+    value[i].addEventListener('click', function(){
+        input_value.innerText += this.innerText;
+    });
+}
+
+for(var i = 0; i<operator.length; i++) {
+    operator[i].addEventListener('click', function(){
+        if (input_value.innerText !== "") {
+            if (output_value.innerText === "") {
+                output_value.innerText = input_value.innerText + " " + this.innerText;
+            } else {
+                output_value.innerText = output_value.innerText + " " + input_value.innerText + " " + this.innerText;
+            }
+        }
+        if (input_value.innerText === "" && output_value.innerText !== "") {
+            output_value.innerText = output_value.innerText.slice(0, -1) + " " + this.innerText;
+        }
+        empty(input_value);
+    });
+}
+
+document.getElementById('equal').addEventListener('click', function(){
+    if (input_value.innerText !== "") {
+        input_value.innerText = eval(output_value.innerText + input_value.innerText);
+        empty(output_value);
+    }else{
+        return;
+    }
+});
+
+
