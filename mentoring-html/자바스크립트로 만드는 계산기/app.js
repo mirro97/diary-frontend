8936260a90cf7fca1c7c2caecd63a61ad20d5d7f
NYchicken <mirro97@naver.com>
2020-12-10 03:36:04 +0900

    mini project: js-calculator add css style

--- mentoring-html/자바스크립트로 만드는 계산기/app.js
+++ mentoring-html/자바스크립트로 만드는 계산기/app.js
@@ -1,54 +1,56 @@
-const output_value = document.getElementById('output-display');
-const input_value = document.getElementById('input-display');
-const value = document.querySelectorAll('.number');
-const operator = document.querySelectorAll('.operator');
 
-const empty = (element) =>{
-    element.innerText = "";
-};
 
-document.getElementById('claer').addEventListener('click', function(){
-    empty(output_value);
-    empty(input_value);
-});
+// const output_value = document.getElementById('output-display');
+// const input_value = document.getElementById('input-display');
+// const value = document.querySelectorAll('.number');
+// const operator = document.querySelectorAll('.operator');
 
-document.getElementById('claer').addEventListener('click', function(){
-    input_value.innerText = "";
-});
+// const empty = (element) =>{
+//     element.innerText = "";
+// };
 
-document.getElementById('dot').addEventListener('click', function(){
-    if(input_value.innerText.includes('.')) return;
+// document.getElementById('claer').addEventListener('click', function(){
+//     empty(output_value);
+//     empty(input_value);
+// });
 
-    input_value.innerText += this.innerText;
-});
+// document.getElementById('claer').addEventListener('click', function(){
+//     input_value.innerText = "";
+// });
 
-for(var i = 0; i<value.length; i++) {
-    value[i].addEventListener('click', function(){
-        input_value.innerText += this.innerText;
-    });
-}
+// document.getElementById('dot').addEventListener('click', function(){
+//     if(input_value.innerText.includes('.')) return;
 
-for(var i = 0; i<operator.length; i++) {
-    operator[i].addEventListener('click', function(){
-        if (input_value.innerText !== "") {
-            if (output_value.innerText === "") {
-                output_value.innerText = input_value.innerText + " " + this.innerText;
-            } else {
-                output_value.innerText = output_value.innerText + " " + input_value.innerText + " " + this.innerText;
-            }
-        }
-        if (input_value.innerText === "" && output_value.innerText !== "") {
-            output_value.innerText = output_value.innerText.slice(0, -1) + " " + this.innerText;
-        }
-        empty(input_value);
-    });
-}
+//     input_value.innerText += this.innerText;
+// });
 
-document.getElementById('equal').addEventListener('click', function(){
-    if(input_value.innerText !== "") {
-        input_value.innerText = eval(output_value.innerText + input_value.innerText);
-        empty(output_value);
-    }
-});
+// for(var i = 0; i<value.length; i++) {
+//     value[i].addEventListener('click', function(){
+//         input_value.innerText += this.innerText;
+//     });
+// }
+
+// for(var i = 0; i<operator.length; i++) {
+//     operator[i].addEventListener('click', function(){
+//         if (input_value.innerText !== "") {
+//             if (output_value.innerText === "") {
+//                 output_value.innerText = input_value.innerText + " " + this.innerText;
+//             } else {
+//                 output_value.innerText = output_value.innerText + " " + input_value.innerText + " " + this.innerText;
+//             }
+//         }
+//         if (input_value.innerText === "" && output_value.innerText !== "") {
+//             output_value.innerText = output_value.innerText.slice(0, -1) + " " + this.innerText;
+//         }
+//         empty(input_value);
+//     });
+// }
+
+// document.getElementById('equal').addEventListener('click', function(){
+//     if(input_value.innerText !== "") {
+//         input_value.innerText = eval(output_value.innerText + input_value.innerText);
+//         empty(output_value);
+//     }
+// });
 
 
