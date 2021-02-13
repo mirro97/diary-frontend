da9402de15818b8be927e67f3c5d0461d5cbd227
NYchicken <mirro97@naver.com>
2021-02-14 03:23:54 +0900

    React with Scss - button 스타일링

--- React/styling-with-sass/src/App.js
+++ React/styling-with-sass/src/App.js
@@ -1,11 +1,68 @@
 import React from "react";
-import Button from "./components/Button";
 import "./App.scss";
+import Button from "./components/Button";
+
 function App() {
   return (
     <div className="App">
       <div className="buttons">
+        <Button size="large">BUTTON</Button>
         <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color="lime" size="large">
+          BUTTON
+        </Button>
+        <Button color="lime">BUTTON</Button>
+        <Button color="lime" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="yellow" size="large">
+          BUTTON
+        </Button>
+        <Button color="yellow">BUTTON</Button>
+        <Button color="yellow" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline>
+          BUTTON
+        </Button>
+        <Button color="lime" outline>
+          BUTTON
+        </Button>
+        <Button color="yellow" size="small" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth className="customized-button">
+          BUTTON
+        </Button>
+        <Button
+          color="lime"
+          size="large"
+          fullWidth
+          onClick={() => {
+            console.log("클릭!");
+          }}
+        >
+          BUTTON
+        </Button>
+        <Button
+          color="yellow"
+          size="large"
+          fullWidth
+          onMouseMove={() => {
+            console.log("마우스 무브!");
+          }}
+        >
+          BUTTON
+        </Button>
       </div>
     </div>
   );
--- React/styling-with-sass/src/components/Button.js
+++ React/styling-with-sass/src/components/Button.js
@@ -1,8 +1,38 @@
 import React from "react";
 import "./Button.scss";
+import classNames from "classnames";
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+// size: large, medium, small
+// color: blue, lime, yellow
+function Button({
+  children,
+  size,
+  color,
+  outline,
+  fullWidth,
+  className,
+  ...rest
+}) {
+  console.log(rest);
+  return (
+    <button
+      className={classNames(
+        "Button",
+        size,
+        color,
+        { outline, fullWidth },
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
+
+Button.defaultProps = {
+  size: "medium",
+  color: "blue",
+};
 
 export default Button;
