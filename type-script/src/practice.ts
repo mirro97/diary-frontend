8cebf59fecbd104a61446cfa8c8a35443dba553b
NYchicken <mirro97@naver.com>
2021-02-27 02:30:21 +0900

    TypeScript - Generics 연습해보기

--- type-script/src/practice.ts
+++ type-script/src/practice.ts
@@ -1,24 +1,56 @@
-type Person = {
-    name: string;
-    age?: number; 
-  };
-  
-  type Developer = Person & {
-    skills: string[];
-  };
-  
-  const person: Person = {
-    name: '김사람'
-  };
-  
-  const expert: Developer = {
-    name: '김개발',
-    skills: ['javascript', 'react']
-  };
-  
-  type People = Person[];
-  const people: People = [person, expert];
-  
-  type Color = 'red' | 'orange' | 'yellow';
-  const color: Color = 'red';
-  const colors: Color[] = ['red', 'orange'];
+// ex1
+function merge<T1, T2> (a: T1, b: T2){
+    return{
+        ...a, ...b
+    };
+}
+
+const merged = merge({foo: 1}, {bar: 2, foobar: 3});
+
+// ex2
+function wrap<T>(param: T){
+    return {
+        param
+    };
+}
+
+const wrapped = wrap('ppp');
+
+// ex3
+type Items<T, V> = {
+    list: T[];
+    value: V
+};
+
+const items: Items<number, string> = {
+    list: [1, 2, 3],
+    value: 'aaa',
+};
+
+// ex4
+class Queue<T> {
+    list: T[] = [];
+
+    get length(){
+        return this.list.length;
+    }
+
+    enqueue(item: T) {
+        this.list.push(item);
+    }
+
+    dequeue(){
+        return this.list.shift();
+    }
+}
+
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+
+while(queue.length > 0){
+    console.log(queue.dequeue());
+}
